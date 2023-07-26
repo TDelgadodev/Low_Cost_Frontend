efe0e8e18d18df35b1414984be749ce95ebd6b2c
Thiago <thiagodelgado.dev@gmail.com>
2023-07-26 13:52:07 -0300

    add register mobile done

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,89 @@
+import { Button, Form } from "react-bootstrap";
+import * as Yup from "yup";
+import { Field, Formik } from "formik";
+import styles from "./index.module.css";
+import { Link } from "react-router-dom";
+
+export const Login = () => {
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().required("Debe ingresar un email"),
+    password: Yup.string().required("La contraseña es obligatoria"),
+  });
+
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={validationSchema}
+    >
+      {(formik) => (
+        <Form onSubmit={formik.handleSubmit} className="m-2">
+          <div>
+            <h2>Hola, Bienvenido!</h2>
+          </div>
+          <Form.Group className="mb-2">
+            <Form.Label htmlFor="email">Email</Form.Label>
+            <Field
+              id="email"
+              type="text"
+              placeholder="Ingresa tu email"
+              name="email"
+              as={Form.Control}
+              className="shadow border-secondary"
+            ></Field>
+          </Form.Group>
+          <Form.Group className="mb-2 form-box">
+            <Form.Label htmlFor="password">Contraseña</Form.Label>
+            <Field
+              id="password"
+              type="password"
+              placeholder="Ingresa su contraseña"
+              name="password"
+              as={Form.Control}
+              className="shadow border-secondary"
+            ></Field>
+            <button className={styles.formBox}>
+            <i className="fa-solid fa-eye"></i>
+            </button>
+          </Form.Group>
+          {["checkbox"].map((type) => (
+            <div key={`default-${type}`} className=" d-flex w-100 gap-2">
+              <Form.Check // prettier-ignore
+                type={type}
+                className="m-0 "
+                id={`default-${type}`}
+                label={"Recuerdame"}
+              />
+              <span className="text-danger">Olvido su contraseña?</span>
+            </div>
+          ))}
+          <div className="mt-3">
+            <div className={`d-grid gap-2`}>
+              <Button
+                variant="primary"
+                className="w-80 p-2"
+                size="lg"
+                type="submit"
+              >
+                Ingresar
+              </Button>
+            </div>
+          </div>
+          <div className="d-flex gap-2 mt-3">
+            <p className={styles.title}>¿No tienes una cuenta?</p>
+            <Link className="text-decoration-none " to={'/register'}>Registrate</Link>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  );
+};
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from "react-router-dom";
-import { Home } from "../pages/Home";
+import { Home } from "../pages/home";
+import { Login } from "../pages/Login";
+import { Register } from "../pages/Register";
+
 
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
