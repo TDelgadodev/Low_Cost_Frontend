2c182b315fca9d660cac7173fa209e32d045e705
Thiago <thiagodelgado.dev@gmail.com>
2023-07-28 10:57:21 -0300

    views on tablet done

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,6 +3,7 @@ import * as Yup from "yup";
 import { ErrorMessage, Field, Formik } from "formik";
 import styles from "./index.module.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const Login = () => {
   const initialValues = {
@@ -19,6 +20,8 @@ export const Login = () => {
     console.log(values);
   };
 
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <Formik
       initialValues={initialValues}
@@ -27,7 +30,14 @@ export const Login = () => {
     >
       {(formik) => (
         <Form onSubmit={formik.handleSubmit}>
-          <Col xs={10} sm={6} md={6} lg={4} xl={4} className={`${styles.loginForm} mb-5`}>
+          <Col
+            xs={10}
+            sm={6}
+            md={6}
+            lg={4}
+            xl={4}
+            className={`${styles.loginForm} mb-5`}
+          >
             <div>
               <h2>Hola, Bienvenido!</h2>
             </div>
@@ -40,58 +50,72 @@ export const Login = () => {
                 name="email"
                 as={Form.Control}
                 className="shadow border-secondary"
+                onFocus={() => formik.setFieldError("email", "")}
               ></Field>
               <ErrorMessage
-                name='email'
+                name="email"
                 component={Form.Text}
-                className="text-danger ms-2"
+                className="text-danger"
               ></ErrorMessage>
             </Form.Group>
-            <Form.Group className="mb-2 form-box">
+            <Form.Group className={`mb-2 form-box ${styles.forxTablet}`}>
               <Form.Label htmlFor="password">Contraseña</Form.Label>
               <Field
                 id="password"
-                type="password"
+                type={showPassword ? "text" : "password"}
                 placeholder="Ingresa su contraseña"
                 name="password"
                 as={Form.Control}
                 className="shadow border-secondary"
               ></Field>
               <ErrorMessage
-                name="password"
+                name="email"
                 component={Form.Text}
-                className="text-danger ms-2"
+                className="text-danger"
               ></ErrorMessage>
-              <button className={styles.formBox}>
-                <i className="fa-solid fa-eye"></i>
+              <button
+                type="button"
+                className={styles.formBox}
+                onClick={() => setShowPassword((prev) => !prev)}
+              >
+                <i
+                  className={`fa-solid fa-eye${showPassword ? "-slash" : ""}`}
+                ></i>
               </button>
             </Form.Group>
             {["checkbox"].map((type) => (
-              <div key={`default-${type}`} className=" d-flex w-100 gap-2">
+              <div
+                key={`default-${type}`}
+                className={`d-flex w-100 gap-2 ${styles.formBoxLInksTablet}`}
+              >
                 <Form.Check
                   type={type}
                   className="m-0 "
                   id={`default-${type}`}
                   label={"Recuerdame"}
                 />
-                <Link className="text-danger text-decoration-none" to={'/resetPassword'}>Olvido su contraseña?</Link>
+                <Link
+                  className="text-danger text-decoration-none white-space-nowrap"
+                  to={"/resetPassword"}
+                >
+                  Olvido su contraseña?
+                </Link>
               </div>
             ))}
             <div className="mt-3">
-              <div className={`d-grid gap-2`}>
+              <div className={`d-grid gap-2 ${styles.btnTablet}`}>
                 <Button
                   variant="primary"
-                  className="w-80 p-2"
-                  size="lg"
+                  className="w-100 p-2 mx-auto"
                   type="submit"
                 >
                   Ingresar
                 </Button>
               </div>
             </div>
-            <div className="d-flex gap-2 mt-3">
+            <div className={`d-flex gap-2 mt-3 ${styles.formBoxLinks}`}>
               <p className={styles.title}>¿No tienes una cuenta?</p>
-              <Link className="text-decoration-none " to={"/register"}>
+              <Link className="text-decoration-none" to={"/register"}>
                 Registrate
               </Link>
             </div>
