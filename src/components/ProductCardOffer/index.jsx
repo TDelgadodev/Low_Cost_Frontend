2685c785d8ab94a1d5660cb31376d41f8ad5f0be
gnicolaslan <gabrielnicolas.dg@gmail.com>
2023-07-31 22:43:41 -0300

    fix home offer images

--- src/components/ProductCardOffer/index.jsx
+++ src/components/ProductCardOffer/index.jsx
@@ -4,6 +4,7 @@ import { useCart } from '../../hooks/useCart'
 import styles from './ProductCardOffer.module.css'
 
 export default function ProductCardOffer({ product }) {
+    
     /* console.log("Product in ProductCardOffer:", product); */
 
     /*     const { handleModalClick, handleProductIdClick } = useProducts() */
@@ -12,10 +13,13 @@ export default function ProductCardOffer({ product }) {
     function handleAddToCart(product) {
         addToCart(product)
     }
+    /* console.log("lista de productos en ofertA", product) */
+
+    const imageUrl = product.imageUrls && product.imageUrls[0] ? product.imageUrls[0] : 'nofoto.png';
 
     return (
         <Card>
-            <Card.Img variant="top" src={product.imageUrls[0]}
+            <Card.Img variant="top" src={imageUrl}
                 className={`${styles.cardImg}`} />
             <Card.Body>
                 <Card.Title>{product.name}</Card.Title>
@@ -34,7 +38,7 @@ export default function ProductCardOffer({ product }) {
 
 ProductCardOffer.propTypes = {
     product: PropTypes.shape({
-        imageUrls: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]).isRequired,
+        imageUrls: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
         name: PropTypes.string.isRequired,
         price: PropTypes.number.isRequired,
         id: PropTypes.number.isRequired
