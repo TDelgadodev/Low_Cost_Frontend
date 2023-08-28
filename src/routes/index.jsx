5a930f93cef52e4e7180570cb73378a23d1a03b4
gnicolaslan <gabrielnicolas.dg@gmail.com>
2023-08-28 17:10:47 -0300

    fix routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -8,8 +8,8 @@ import { Detail } from "../pages/Detail";
 import { Profile } from "../pages/Profile";
 import ProtectedRoutes from "./protectedRoutes";
 import useAuth from "../hooks/useAuth";
-/* import GetCodeResetMain from "../pages/GetCodeReset/GetCodeReset";
- */
+import { GetCodeResetMain } from "../pages/GetCodeReset/GetCodeReset";
+
 
 export const AppRoutes = () => {
   const { user } = useAuth();
@@ -26,8 +26,8 @@ export const AppRoutes = () => {
       >
         <Route path="/profile/:id" element={<Profile />} />
       </Route>
-{/*       <Route path="/get-code" element={<GetCodeResetMain/>} />
- */}      <Route path="/finish-buying" element={<CompletedPurchase />} />
+      <Route path="/get-code" element={<GetCodeResetMain />} />
+      <Route path="/finish-buying" element={<CompletedPurchase />} />
     </Routes>
   );
 };
