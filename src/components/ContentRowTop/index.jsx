8986de4acb03404eceb2b70a9c1fd208467865ba
Thiago <thiagodelgado.dev@gmail.com>
2023-08-31 15:19:17 -0300

    add dashboard in progress

---
+++ src/components/ContentRowTop/index.jsx
@@ -0,0 +1,5 @@
+export const ContentRowTop = () => {
+  return (
+    <div>ContentRowTop</div>
+  )
+}
---
+++ src/components/ContentWrapper/index.jsx
@@ -0,0 +1,5 @@
+export const Contentwrapper = () => {
+  return (
+    <div>Contentwrapper</div>
+  )
+}
