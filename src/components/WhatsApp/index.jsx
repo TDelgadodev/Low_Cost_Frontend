d79bb4eebefeeae8e5acb6dcacc8fbbca72e1c19
gnicolaslan <gabrielnicolas.dg@gmail.com>
2023-11-24 15:58:16 -0300

    whatsapp fix number

--- src/components/WhatsApp/index.jsx
+++ src/components/WhatsApp/index.jsx
@@ -2,7 +2,7 @@ import styles from './WhatsApp.module.css'
 
 export default function WhatsApp() {
     return (
-        <a href="https://web.whatsapp.com/" target="_blank" rel='noreferrer'>
+        <a href="https://wa.link/b9xkam" target="_blank" rel='noreferrer'>
             <img src="/wsp-logo.png" alt="wsp" className={`${styles.whatsAppLogo}`} /></a>
             
     )
